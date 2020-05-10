f31c85ae6417a944da024479b9532bda857c2d2b
Guihution <994233704@qq.com>
2020-05-10 17:19:04 +0800

    05101719
    05101719

--- pages/broadcast/broadcastList.js
+++ pages/broadcast/broadcastList.js
@@ -1,43 +1,55 @@
-// pages/broadcast/broadcastList.js
-
 import { Config } from '../../utils/config.js';
 import { Ljrqe } from '../../utils/ljrqe.js';
 
 var ljrqe = new Ljrqe();
- // "plugins":{
-  //   "live-player-plugin":{
-  //     "version": "1.0.3",
-  //   }
-  // },
+let livePlayer = requirePlugin('live-player-plugin');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    bannerList:[]
-  },
-
-  getBannerList() {
-    let data = { };
-
-    ljrqe.post('banner/list', data).then(res => {
-      let list = res.data;
-      this.setData({
-        bannerList: list
-      })
-    })
+    liveList: [],
+    statusArr: { 101: "直播中", 102: "未开始", 103: "已结束", 104: "禁播", 105: "暂停中", 106: "异常", 107: "已过期" },
+    interval:0,
+    timer:60000,
   },
 
   getList() {
     ljrqe.post('live/list', {}).then(res => {
-      console.log(res)
       let list = res.room_info;
-      console.log("live", list)
+      // console.log("live", list)
       this.setData({
         liveList: list
-      })
+      });
+      this.getStatus();
     })
+  },
+  gotoRoom: function(e){
+    let roomId = e.currentTarget.dataset.id; // 具体的房间号，
+    let customParams = encodeURIComponent(JSON.stringify({ path: 'pages/index/index', pid: 1 })) 
+    // 开发者在直播间页面路径上携带自定义参数（如示例中的path和pid参数）
+    console.log(roomId,customParams);
+    wx.navigateTo({
+      url: `plugin-private://wx2b03c6e691cd7370/pages/live-player-plugin?room_id=${roomId}&custom_params=${customParams}`
+    })
+  },
+  getStatus(){
+    let t = this,d=t.data, liveList = d.liveList, timer = d.timer;
+    setInterval(function () { 
+      liveList.forEach((v, i) => {
+        livePlayer.getLiveStatus({ room_id: v.roomid })
+          .then(res => {
+            liveList[i].live_status = res.liveStatus;
+          })
+          .catch(err => {
+            console.log('get live status', err)
+          }).then(res => {
+            t.setData({ liveList : liveList })
+          })
+      })
+    }, timer);
   },
   /**
    * 生命周期函数--监听页面加载
@@ -57,7 +69,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.getBannerList();
     this.getList();
   },
 
